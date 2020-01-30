745a8ff35a2f53c21aa9aa03884e6dd8d1515c41
Will Kirkhope <will@mototvnetwork.com>
2020-01-29 20:45:22 -0700

    Removed Semantic-Ui card as the amount of styling you could use was limited.

--- outtoeatv2/src/components/RestaurantCard.js
+++ outtoeatv2/src/components/RestaurantCard.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react'
-import { Button } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 
 class RestaurantCard extends Component{
   render(){
     return (
-      <div className='displayedCard'>
-        <Card fluid color='black'>
-          <Image className="ui centered large image" src='/logo192.png' wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>Restaurant One</Card.Header>
-            <Card.Meta>
-              <span className='date'>Added in 2020</span>
-            </Card.Meta>
-            <Card.Description>
-              This is where you will be eating deal with it.
-            </Card.Description>
-          </Card.Content>
-        </Card>
+      <div className='mainDisplay'>
+        <div className='displayedCard'>
+          <img src='/logo192.png'/>
+          <h3>Restaurant One</h3>
+          <span>Added in 2020</span>
+          <p>This is where you will be eating, deal with it.</p>
+        </div>
         <Button color='black' icon='shuffle'>
           <Button.Content visible></Button.Content>
           <Button.Content hidden>
